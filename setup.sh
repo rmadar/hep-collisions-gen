ba3b916b2bd392710e3f797cad141c38e120dfd6
rmadar <romain.madar@gmail.com>
2021-02-15 14:43:34 +0100

    Adding setup file

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# ---------------------------------------
+# CAUTION: this file is system dependent.
+# ---------------------------------------
+
+# Setup the right ROOT version, compatible with Delphes
+source /opt/root/root-6.14.04/bin/thisroot.sh
