ba54ccbcfdcd0a159a642de59648b60cfc03cb2e
rmadar <romain.madar@gmail.com>
2021-02-19 17:04:29 +0100

    Update the ttbar_dilep script

--- generate/ttbar_dilep.py
+++ generate/ttbar_dilep.py
@@ -2,11 +2,10 @@ import generation_handler as gen
 
 # Defning the two types of collision under study
 fs = 't t~, (t > b W+, W+ > l+ vl), (t~ > b~ W-, W- > l- vl~)'
-pp13TeV  = gen.collision('p p'    , fs, ebeam1='6500', ebeam2='6500')
-pp100TeV = gen.collision('p p'    , fs, ebeam1='50000', ebeam2='50000')
-ee       = gen.collision('mu+ mu-', fs, ebeam1='180' , ebeam2='180')
-mm       = gen.collision('e+ e-'  , fs, ebeam1='180' , ebeam2='180')
-tt       = gen.collision('ta+ ta-', fs, ebeam1='180' , ebeam2='180')
+pp = gen.collision('p p'    , fs, ebeam1='6500', ebeam2='6500')
+ee = gen.collision('e+ e-'  , fs, ebeam1='180' , ebeam2='180')
+mm = gen.collision('mu+ mu-', fs, ebeam1='180' , ebeam2='180')
+tt = gen.collision('ta+ ta-', fs, ebeam1='180' , ebeam2='180')
 
 # Get a MadGraph instance
 mg = gen.madgraph('../MG5_aMC')
@@ -17,9 +16,9 @@ mg.run_proc_dir(proc=ee.mg_proc(), directory='ee_ttbar')
 mg.run_proc_dir(proc=mm.mg_proc(), directory='mm_ttbar')
 mg.run_proc_dir(proc=tt.mg_proc(), directory='tt_ttbar')
 
-# Generate events for the LHC
-mg.gen_evts('pp_ttbar', 'run01', pp.beam_params())
-mg.gen_evts('ee_ttbar', 'run01', ee.beam_params())
-mg.gen_evts('mm_ttbar', 'run01', mm.beam_params())
-mg.gen_evts('tt_ttbar', 'run01', tt.beam_params())
+# Generate events
+mg.gen_evts('pp_ttbar', 'run02', pp.beam_params(), pythia=True, delphes=True)
+mg.gen_evts('ee_ttbar', 'run02', ee.beam_params(), pythia=True, delphes=True)
+mg.gen_evts('mm_ttbar', 'run02', mm.beam_params(), pythia=True, delphes=True)
+mg.gen_evts('tt_ttbar', 'run02', tt.beam_params(), pythia=True, delphes=True)
 
